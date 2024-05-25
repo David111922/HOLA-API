34b57ad2154255ec75a02a199c477cda2427ee40
David111922 <salve62@yahoo.com>
2024-05-25 15:36:55 -0700

    setting up API

---
+++ index.js
@@ -0,0 +1,18 @@
+// Require needed modules
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res) =>
+ {
+res.send('See node API')
+
+ })
+
+
+const mongoose = require("mongoose");
+console.log ('See Me')
+
+
+app.listen(3000, () => {
+console.log('Node is running on port 300')
+})
